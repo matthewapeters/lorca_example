21256123429d3ea04b2d9fd64022bbc94fbe3cbd
matthewapeters <matthewapeters@comcast.net>
2020-11-17 16:58:07 -0800

    better

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module lorca_example
 go 1.14
 
 require (
-	github.com/matthewapeters/dali v0.0.0-20201117143821-2ff6e2d28cfa
+	github.com/matthewapeters/dali v0.0.3
 	github.com/zserge/lorca v0.1.9
 )
--- vendor/github.com/matthewapeters/dali/pane.go
+++ vendor/github.com/matthewapeters/dali/pane.go
@@ -5,16 +5,23 @@ import "fmt"
 // Pane is a page within a Window
 type Pane struct {
 	Class    string
-	Name     string
+	ID       string
 	Style    string
 	Elements Elements
 }
 
 // Panes is a map of Pane elements
-type Panes map[string]Pane
+type Panes struct {
+	List []*Pane
+}
+
+// Add a Pane the list of Panes
+func (ps *Panes) Add(p *Pane) {
+	ps.List = append(ps.List, p)
+}
 
 //String for Pane
-func (p Pane) String() string {
+func (p *Pane) String() string {
 	class := ""
 	style := ""
 	if p.Class != "" {
@@ -24,27 +31,32 @@ func (p Pane) String() string {
 		style = fmt.Sprintf(` style="%s"`, p.Style)
 	}
 
-	return fmt.Sprintf(`<div id="%s"%s%s>%s</div>`, p.Name, class, style, p.Elements)
+	return fmt.Sprintf(`<div id="%s"%s%s>%s</div>`, p.Name(), class, style, p.Elements)
 }
 
 //String for Panes
-func (ps Panes) String() string {
+func (ps *Panes) String() string {
 	html := ""
-	for _, p := range ps {
+	for _, p := range ps.List {
 		html = fmt.Sprintf(`%s%s`, p, html)
 	}
 	return html
 }
 
 //AddElement adds an element to a Pane
-func (p Pane) AddElement(el Element) {
-	p.Elements[el.Name()] = el
+func (p *Pane) AddElement(el Element) {
+	p.Elements = append(p.Elements, el)
 }
 
 // NewPane generates a new Pane
-func NewPane(name string) Pane {
-	return Pane{
-		Name:     name,
-		Elements: Elements(map[string]Element{}),
+func NewPane(name string) *Pane {
+	return &Pane{
+		ID:       name,
+		Elements: Elements([]Element{}),
 	}
+}
+
+//Name returns the name of the Pane
+func (p *Pane) Name() string {
+	return p.ID
 }
